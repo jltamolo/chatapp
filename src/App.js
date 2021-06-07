5567b3a8f5e57e1d33b0c5b35fae6ff03f60e041
jltamolo <jltamolo@gmail.com>
2021-06-07 20:02:31 +0300

    chatengine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <ChatEngine
+
+        height="100vh"
+        projectID="b57763c0-667d-4048-b20e-a420072a3600"
+        userName="jane"
+        userSecrret="jane123"
+        />
+    );
 }
 
 export default App;
