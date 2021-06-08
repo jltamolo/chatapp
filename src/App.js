8e9b2ede019483659cbe9071082f3c1274d32e99
Julie <48333925+jltamolo@users.noreply.github.com>
2021-06-08 09:28:04 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
+import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
-import './App.css';
 
-const App = () => {
-    return (
-        <ChatEngine
-
-        height="100vh"
-        projectID="b57763c0-667d-4048-b20e-a420072a3600"
-        userName="jane"
-        userSecrret="jane123"
-        />
-    );
+export function App() {
+	return (
+		<ChatEngine
+			height='100vh'
+			userName='jane'
+			userSecret='jane123'
+			projectID='b57763c0-667d-4048-b20e-a420072a3600'
+		/>
+	);
 }
-
-export default App;
